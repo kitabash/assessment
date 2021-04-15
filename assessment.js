2ac21e5e6d30c6408a54be9cb85fbdaf0f56dd17
kitabash <82597760+kitabash@users.noreply.github.com>
2021-04-16 00:14:23 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -33,23 +33,23 @@ assessmentButton.onclick = () => {
     paragraph.innerText = result;
     resultDivided.appendChild(paragraph);
 
-    // ツイートエリアの作成　
-    removeAllChildren(tweetDivided);
-    const anchor = document.createElement('a');
-    const hrefValue = 
-        'https://twitter.com/intent/tweet?button_hashtag=' + 
-        encodeURIComponent('あなたのいいところ') + 
+    // ツイートエリアの作成
+      removeAllChildren(tweetDivided);
+      const anchor = document.createElement('a');
+      const hrefValue =
+        'https://twitter.com/intent/tweet?button_hashtag=' +
+        encodeURIComponent('あなたのいいところ') +
         '&ref_src=twsrc%5Etfw';
-    anchor.setAttribute('href', hrefValue);
-    anchor.className = 'twitter-hashtag-button'
-    anchor.setAttribute('data-text', result);
-    anchor.innerText = 'Tweet #あなたのいいところ';
-    tweetDivided.appendChild(anchor);
+      anchor.setAttribute('href', hrefValue);
+      anchor.className = 'twitter-hashtag-button';
+      anchor.setAttribute('data-text', result);
+      anchor.innerText = 'Tweet #あなたのいいところ';
+      tweetDivided.appendChild(anchor);
 
-    // widgets.js の設定
-    const script = document.createElement('script');
-    script.setAttribute('src','http://platform.twitter.com/widgets.js');
-    tweetDivided.appendChild(script);
+      // widgets.js の設定
+      const script = document.createElement('script');
+      script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+      tweetDivided.appendChild(script);
 };
 
 const answers = [
